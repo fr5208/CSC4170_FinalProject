9e810e80c49ad407c40e7d51a7c767fa9b44d50e
Evan Melvin <fr5208@wayne.edu>
2018-06-11 23:26:22 -0400

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,211 @@
+import java.sql.*;
+public class Main
+{
+	private static ResultSet resultSet = null;
+	private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/sampledb?user=john&password=pass1234";
+	private static final String USER = "john";
+	private static final String PASS = "pass1234";
+
+	static DispGUI gui = new DispGUI();
+	
+	public static void initDb()
+	{
+		Connection connection = null;
+		Statement statement = null;
+		
+		try
+		{
+			//JDBC Driver
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//Open Connection
+			connection = DriverManager.getConnection(DB_URL, USER, PASS);
+			statement = connection.createStatement();
+			
+			//Initialize the database tables
+			statement.executeUpdate("CREATE TABLE PCMembers (Username VARCHAR(32), Password VARCHAR(32), PRIMARY KEY(Username))");
+			statement.executeUpdate("CREATE TABLE Papers (PaperID INT NOT NULL AUTO_INCREMENT, Title VARCHAR(64), Abstract TEXT, Pdf BLOB, PRIMARY KEY(PaperID))");
+			statement.executeUpdate("CREATE TABLE Authors (Name VARCHAR(32), Email VARCHAR(64), University VARCHAR(64), PRIMARY KEY(Name))");
+			statement.executeUpdate("CREATE TABLE ReviewReports (ReviewID INT NOT NULL, Description TEXT, Recommendation VARCHAR(8), PRIMARY KEY(ReviewID))");
+			statement.executeUpdate("CREATE TABLE PCMemberPapers (PaperID INT, PCMember VARCHAR(32), FOREIGN KEY (PaperID) REFERENCES Papers(PaperID), FOREIGN KEY (PCMember) REFERENCES PCMembers(Username))");
+			statement.executeUpdate("CREATE TABLE AuthorPapers (PaperID INT, Author VARCHAR(32), FOREIGN KEY (PaperID) REFERENCES Papers(PaperID), FOREIGN KEY (Author) REFERENCES Authors(Name))");
+
+			
+			//Add records
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('John', 'pass1234')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('James', 'testpass')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('Mary', '1234')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('Jim', '54321')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('Steve', '121212')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('Bob', '000000')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('Linus', '010101')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('Richard', '101010')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('Alan', '435621')");
+			statement.executeUpdate("INSERT INTO PCMembers (Username, Password) " + " VALUES ('Ada', 'testpass2')");
+			
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 1')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 2')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 3')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 4')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 5')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 6')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 7')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 8')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 9')");
+			statement.executeUpdate("INSERT INTO Papers (Title) " + " VALUES ('Paper 10')");
+			
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('John')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('James')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('Mary')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('Jim')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('Steve')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('Bob')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('Linus')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('Richard')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('Alan')");
+			statement.executeUpdate("INSERT INTO Authors (Name) " + " VALUES ('Ada')");
+
+
+
+		}
+		catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if(statement != null)
+					statement.close();
+			}
+			catch(SQLException se2){}
+			try
+			{
+				if(connection != null)
+					connection.close();
+			}
+			catch(SQLException se3)
+			{
+				se3.printStackTrace();
+			}
+		}
+	}
+	
+	public static int dbLogin()
+	{
+		int retVal = 0;
+		Connection connection = null;
+		Statement statement = null;
+		String loginQuery = 	"SELECT * FROM PCMembers " +
+								"WHERE Username='" + gui.getLogin() + "'" +
+								" AND Password='" + gui.getPass() + "'";
+		
+		try
+		{
+			//JDBC Driver
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//Open Connection
+			connection = DriverManager.getConnection(DB_URL, USER, PASS);
+			statement = connection.createStatement();
+			
+			ResultSet results = statement.executeQuery(loginQuery);
+			
+			if(results.next())
+				retVal = 1;
+		
+		}
+		catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if(statement != null)
+					statement.close();
+			}
+			catch(SQLException se2){}
+			try
+			{
+				if(connection != null)
+					connection.close();
+			}
+			catch(SQLException se3)
+			{
+				se3.printStackTrace();
+			}
+		}
+		
+		return retVal;
+	}
+	
+	public static void assignAuthors()
+	{
+		Connection connection = null;
+		Statement statement = null;
+		String author1 = gui.getAuthor1();
+		String author2 = gui.getAuthor2();
+		String author3 = gui.getAuthor3();
+		String paperID = gui.getPaperID();
+		
+		try
+		{
+			//JDBC Driver
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//Open Connection
+			connection = DriverManager.getConnection(DB_URL, USER, PASS);
+			statement = connection.createStatement();
+			
+			statement.executeUpdate("INSERT INTO AuthorPapers (PaperID, Author) " + "VALUES ('" + paperID + "', '" + author1 + "')" );
+			statement.executeUpdate("INSERT INTO AuthorPapers (PaperID, Author) " + "VALUES ('" + paperID + "', '" + author2 + "')" );
+			statement.executeUpdate("INSERT INTO AuthorPapers (PaperID, Author) " + "VALUES ('" + paperID + "', '" + author3 + "')" );
+			
+		
+		}
+		catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if(statement != null)
+					statement.close();
+			}
+			catch(SQLException se2){}
+			try
+			{
+				if(connection != null)
+					connection.close();
+			}
+			catch(SQLException se3)
+			{
+				se3.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+
+	}
+}
